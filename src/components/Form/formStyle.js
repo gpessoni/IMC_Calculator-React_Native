8454e45872e32afe5a7671be7e62d5fac7795607
gpessoni <pessonigs@gmail.com>
2022-06-03 11:34:49 -0300

    Create the styles page for the app

---
+++ src/components/Form/formStyle.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  formContext: {
+    width: "100%",
+    height: "100%",
+    bottom: 0,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 30,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  form: {
+    width: "100%",
+    height: "auto",
+    padding: 10,
+    marginTop: 30,
+  },
+  formLabel: {
+      color: "#000",
+      fontSize:18,
+      paddingLeft:20,
+  },
+});
+
+export default styles;
