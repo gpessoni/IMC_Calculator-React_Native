f53675c775e08862ce5e76b3ac5566d16516821d
gpessoni <pessonigs@gmail.com>
2022-06-03 11:33:58 -0300

    Update the Title from the App

--- src/components/Title/Index.js
+++ src/components/Title/Index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./titleStyle";
 
 export default function Title() {
   return (
-    <View>
-      <Text>OneBit Health</Text>
+    <View style={styles.boxTitle}>
+      <Text style={styles.textTitle}>IMC CALCULATOR</Text>
     </View>
   );
 }
