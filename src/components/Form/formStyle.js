3f5f98608432753ed848e153ab8585605110dad4
gpessoni <pessonigs@gmail.com>
2022-06-03 13:52:44 -0300

    Update the Styles from form

--- src/components/Form/formStyle.js
+++ src/components/Form/formStyle.js
@@ -22,6 +22,29 @@ const styles = StyleSheet.create({
       fontSize:18,
       paddingLeft:20,
   },
+  input:{
+    width:'90%',
+    borderRadius: 50,
+    backgroundColor:'#f6f6f6',
+    height: 40,
+    margin: 12,
+    paddingLeft: 10,
+},
+buttonCalculator:{
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent:"center",
+    width:'90%',
+    backgroundColor:'#ff0043',
+    paddingTop: 14,
+    paddingBottom: 14,
+    marginLeft: 12,
+    margin:30
+},
+textButtonCalculator:{
+    fontSize:20,
+    color:'#ffffff',
+},
 });
 
 export default styles;
