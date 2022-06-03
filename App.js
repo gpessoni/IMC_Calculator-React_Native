e35e659f1d652c5debdd0e7e45cb9c7c2df40eae
gpessoni <pessonigs@gmail.com>
2022-06-03 10:00:37 -0300

    Update the importations from the App js page

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
-import Title from "./src/components/Main/Index";
+import Title from "./src/components/Title/Index";
+import Main from "./src/components/Main/Index";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title />
+      <Main />
     </View>
   );
 }
