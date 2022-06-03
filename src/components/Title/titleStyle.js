cf812af5931e730bd72c869676e98f6209dd3c88
gpessoni <pessonigs@gmail.com>
2022-06-03 11:34:36 -0300

    Create the style from the Title

---
+++ src/components/Title/titleStyle.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  boxTitle: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  textTitle: {
+    color: "#ff0043",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
