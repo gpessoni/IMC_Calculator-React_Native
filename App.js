145c276af49147741c65aa10f58d99edd02987e4
gpessoni <pessonigs@gmail.com>
2022-06-03 11:33:26 -0300

    Update the main app page

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet,  View } from "react-native";
 import Title from "./src/components/Title/Index";
 import Main from "./src/components/Main/Index";
 
@@ -14,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#e0e5e5",
+    paddingTop: 80,
   },
 });
