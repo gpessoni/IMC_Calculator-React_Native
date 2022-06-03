e378827be4350e1ba3b3533f776945e5bb6106b2
gpessoni <pessonigs@gmail.com>
2022-06-03 09:58:05 -0300

    Create the inputs from the User information and the main page

---
+++ src/components/Main/Index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import Form from "../Form/Index";
+
+export default function Main() {
+  return (
+    <View>
+      <Form />
+    </View>
+  );
+}
